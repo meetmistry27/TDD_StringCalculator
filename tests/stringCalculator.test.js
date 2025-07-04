bd08225fbd960adafd3110f5dfee14224d78ea67
meetmistry27 <meetpmistry2004@gmail.com>
2025-07-04 17:08:53 +0530

    test: return number itself when one input is passed

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -5,4 +5,8 @@ describe('StringCalculator', () => {
     expect(StringCalculator.add('')).toBe(0);
   });
 
+  it('should return the sum of a single number', () => {
+    expect(StringCalculator.add('5')).toBe(5);
+  });
+
 });
