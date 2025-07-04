92e9da31142f9dd60f00b9c3f9612067e3c1a0ce
meetmistry27 <meetpmistry2004@gmail.com>
2025-07-04 17:22:10 +0530

    feat: add support summing two numbers

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,10 +4,17 @@ const StringCalculator = {
         if (numbers === "") {
             return 0;
         }
-
-        // Refactored for test: return numbeer itself when one input is passed
-        const singleNumber = Number(numbers);
-        return singleNumber;
+    // Refactored for test: return number itself when one input is passed
+    const numArray = numbers.split(",");
+    if (numArray.length === 1) {
+        return Number(numArray[0]);
+        }
+    // Refactored for test: return sum of two numbers when two inputs are passed    
+    if (numArray.length === 2) {
+        const num1 = Number(numArray[0]);
+        const num2 = Number(numArray[1]);
+        return num1 + num2;
+    }
     }
 }
 
