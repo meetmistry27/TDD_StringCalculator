f109fb16c3fb192ad98c52ecb8eaf4b2b92a71a1
meetmistry27 <meetpmistry2004@gmail.com>
2025-07-04 22:14:24 +0530

    feat: support for custom delimeters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,14 +1,19 @@
 const StringCalculator = {
-    // Refactored for test: add('') should return 0
     add(numbers) {
-        if (numbers === "") {
-            return 0;
-        }
-    // Refactored for test: 
-    const numArray = numbers.split(/[\n,]/);
+  if (numbers === "") return 0;
+
+  //delimeters used before comma and newline
+  let delimeter = /[\n,]/;
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimeter = new RegExp(parts[0].substring(2));
+    numbers = parts.slice(1).join(""); // Remaining part is the numbers
+  }
+    const numArray = numbers.split(delimeter);
     const numberList = numArray.map(num => parseInt(num, 10));
     return numberList.reduce((sum, num) => sum + num, 0);
     }
-}
+
+};
 
 module.exports = StringCalculator;
